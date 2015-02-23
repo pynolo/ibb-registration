17a0b13231ec2e47943e3ac04bfbfeccfee51edd
Paolo Tacconi <p.tacconi@giunti.it>
2015-02-23 16:15:18 +0100

    New Resale model

--- database.sql
+++ database.sql
@@ -43,3 +43,15 @@ CREATE TABLE ipn_responses (
 	payment_type varchar(128) NOT NULL,
 	PRIMARY KEY(id)
 );
+
+DROP TABLE IF EXISTS resale;
+CREATE TABLE resale (
+	id int NOT NULL auto_increment,
+	created datetime NOT NULL,
+	expiration datetime NOT NULL,
+	resale_type varchar(4) NOT NULL,
+	message varchar(256) NOT NULL,
+	email varchar(64) NOT NULL,
+	days int(11) NOT NULL,
+	PRIMARY KEY(id)
+);
---
+++ src/main/java/net/tarine/ibb/servlet/EmailToImageServlet.java
@@ -0,0 +1,67 @@
+package net.tarine.ibb.servlet;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ * @author Abhijit Ghosh
+ * @version 1.0
+ */
+public class EmailToImageServlet extends HttpServlet {
+    private static final long serialVersionUID = -1761346889117186607L;
+ 
+    /* (non-Javadoc)
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // parameters
+    	String username = request.getParameter("username");
+    	if (username == null) username = "";
+    	if (username.length() == 0) username = "--";
+    	String domain = request.getParameter("domain");
+    	if (domain == null) domain = "";
+    	if (domain.length() == 0) domain = "--";
+    	
+    	// set mime type as jpg image
+        response.setContentType("image/jpg");
+        ServletOutputStream out = response.getOutputStream();
+ 
+        BufferedImage bufferedImage = new BufferedImage(200, 30, BufferedImage.TYPE_BYTE_INDEXED);
+ 
+        Graphics2D graphics = bufferedImage.createGraphics();
+         
+        // Set back ground of the generated image to white
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, 200, 40);
+ 
+        // set gradient font of text to be converted to image
+        GradientPaint gradientPaint = new GradientPaint(10, 5, Color.BLUE, 20, 10, Color.LIGHT_GRAY, true);
+        graphics.setPaint(gradientPaint);
+        Font font = new Font("sans-serif", Font.BOLD, 15);
+        graphics.setFont(font);
+ 
+        // write email string in the image
+        String email = username+"@"+domain;
+        graphics.drawString(email, 1, 1);
+ 
+        // release resources used by graphics context
+        graphics.dispose();
+ 
+        // encode the image as a JPEG data stream and write the same to servlet output stream  
+        ImageIO.write(bufferedImage, "jpg", response.getOutputStream());  
+ 
+        // close the stream
+        out.close();
+    }
+}
